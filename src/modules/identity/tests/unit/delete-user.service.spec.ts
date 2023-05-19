e060c5b9dd871af5ee32e8de536df5d34811a16f
Kelviny Henrique <kelviny.henrique.ext@infracommerce.com.br>
2023-05-19 09:12:10 -0300

    Create delete user service and test

---
+++ src/modules/identity/tests/unit/delete-user.service.spec.ts
@@ -0,0 +1,71 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserRepository } from '../../repositories/user.repository';
+import { User } from '../../entities/user.entity';
+import { UserProps } from '../../interfaces/user-props';
+import { fa, faker } from '@faker-js/faker';
+import { DeleteUserService } from '../../services/delete-user.service';
+import { CreateUserService } from '../../services/create-user.service';
+import { randomUUID } from 'crypto';
+
+describe('DeleteUserService', () => {
+  let deleteUserService: DeleteUserService;
+  let userRepository: UserRepository;
+  let createUserService: CreateUserService;
+  let createdUser: User;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        DeleteUserService,
+        CreateUserService,
+        {
+          provide: UserRepository,
+          useValue: {
+            create: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    deleteUserService = module.get<DeleteUserService>(DeleteUserService);
+    createUserService = module.get<CreateUserService>(CreateUserService);
+    userRepository = module.get<UserRepository>(UserRepository);
+
+    jest.clearAllMocks(); // Reset all mock function calls
+  });
+
+ it('should create a new user', async () => {
+  const userProps: UserProps = {
+    email: faker.internet.email(),
+    phone: faker.phone.number(),
+    name: faker.internet.userName(),
+    password: faker.internet.password(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  createdUser = new User(userProps);
+
+  jest.spyOn(userRepository, 'create').mockResolvedValue(createdUser);
+
+  const result: User = await createUserService.execute(userProps);
+
+  expect(result).toBe(createdUser);
+  expect(userRepository.create).toHaveBeenCalledWith(expect.objectContaining(userProps));
+});
+
+
+  it('should delete a user', async () => {
+    jest.spyOn(userRepository, 'delete').mockResolvedValue();
+    await deleteUserService.execute(createdUser.id);
+    expect(userRepository.delete).toHaveBeenCalledWith(createdUser.id);
+  });
+
+  it('should try to delete a user that does not exist', async () => {
+    const user_id: string = randomUUID();
+    jest.spyOn(userRepository, 'delete').mockRejectedValue(new Error());
+    await expect(deleteUserService.execute(user_id)).rejects.toThrow();
+    expect(userRepository.delete).toHaveBeenCalledWith(user_id);
+  });
+});
